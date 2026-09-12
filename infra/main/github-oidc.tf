d3aed7e837a24fc83dcc0db4fe92ff43297f0fbc
aine_mbabazi <158444088+aine-mbabazi@users.noreply.github.com>
2026-09-12 14:54:41 +0300

    infra: add GitHub Actions OIDC deploy role, referencing existing account-level OIDC provider

---
+++ infra/main/github-oidc.tf
@@ -0,0 +1,103 @@
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+data "aws_iam_policy_document" "github_actions_assume" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [data.aws_iam_openid_connect_provider.github.arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:job_workflow_ref"
+      values = [
+        "aine-mbabazi/devops-g2-tillflow/.github/workflows/pr.yml@*",
+        "aine-mbabazi/devops-g2-tillflow/.github/workflows/release.yml@*",
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions_deploy" {
+  name               = "${local.name_prefix}-ci-deploy"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume.json
+  tags               = merge(local.common_tags, { service = "ci-cd" })
+}
+
+data "aws_iam_policy_document" "github_actions_permissions" {
+  statement {
+    sid    = "ECRPushPull"
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "ECSDeploy"
+    effect = "Allow"
+    actions = [
+      "ecs:DescribeServices",
+      "ecs:DescribeTaskDefinition",
+      "ecs:RegisterTaskDefinition",
+      "ecs:UpdateService",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid       = "PassRoleToECS"
+    effect    = "Allow"
+    actions   = ["iam:PassRole"]
+    resources = [
+      aws_iam_role.payments_execution.arn,
+      aws_iam_role.payments_task.arn,
+    ]
+  }
+
+  statement {
+    sid    = "TerraformState"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:ListBucket",
+    ]
+    resources = [
+      "arn:aws:s3:::devops-g2-tillflow-tfstate-240462142849",
+      "arn:aws:s3:::devops-g2-tillflow-tfstate-240462142849/*",
+    ]
+  }
+
+  statement {
+    sid       = "TerraformLock"
+    effect    = "Allow"
+    actions   = ["dynamodb:GetItem", "dynamodb:PutItem", "dynamodb:DeleteItem"]
+    resources = ["arn:aws:dynamodb:us-east-2:240462142849:table/devops-g2-tflock"]
+  }
+}
+
+resource "aws_iam_role_policy" "github_actions" {
+  name   = "${local.name_prefix}-ci-deploy-policy"
+  role   = aws_iam_role.github_actions_deploy.id
+  policy = data.aws_iam_policy_document.github_actions_permissions.json
+}
+
+output "github_actions_role_arn" {
+  value = aws_iam_role.github_actions_deploy.arn
+}
